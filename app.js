4d424a109ce15ae5f959f05360dc4f8bf7bc548b
zuoz <zuoz@163.com>
2022-09-14 12:02:38 +0800

    修改

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 // app.js
 App({
   onLaunch: function () {
-  },
-  onShow (options) {
    
-    // wx.hideLoading({
-    //   success: (res) => {
-    //     console.log("加载完成，所以隐藏掉了")
-    //   },
-    // })
   },
+ 
   globalData: {
 
   },
